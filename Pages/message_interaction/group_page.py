d9aecf439f93825becfdcbb5f4c3e005407414ed
荣幸如 <1242872415@qq.com>
2020-04-04 17:53:39 +0800

    更新

--- Pages/message_interaction/group_page.py
+++
@@ -1,9 +0,0 @@
-#——————群组页——————
-from selenium.webdriver.common.by import By
-from base.base_action import BaseAction
-
-class Group(BaseAction):
-    # 群内部的tab页
-    groupTab = By.ID, 'com.huiian.timing:id/tv_tab_title'
-    # tab群聊
-    groupTalk = By.XPATH, '//*[@text="群聊"]'
